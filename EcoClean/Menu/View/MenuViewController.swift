e7cb7a4109a8faa3c3e47a5db454afa75f597cfb
Karen <khachatryankaren606@gmail.com>
2024-12-04 02:08:33 +0400

    implemented menu

--- EcoClean/Menu/View/MenuViewController.swift
+++ EcoClean/Menu/View/MenuViewController.swift
@@ -13,6 +13,7 @@ class MenuViewController: UIViewController {
 
     @IBOutlet var titleLabels: [UILabel]!
     @IBOutlet weak var chartView: PieChart!
+    @IBOutlet weak var completedTasksLabel: UILabel!
     private let viewModel = MenuViewModel.shared
     private var cancellables: Set<AnyCancellable> = []
 
@@ -20,6 +21,7 @@ class MenuViewController: UIViewController {
         super.viewDidLoad()
         setNaviagtionRightButton()
         titleLabels.forEach({ $0.font = .medium(size: 16) })
+        completedTasksLabel.font = .medium(size: 10)
         subscribe()
         viewModel.fetchData()
     }
@@ -35,10 +37,24 @@ class MenuViewController: UIViewController {
                 guard let self = self else { return }
                 let completedTasks = tasks.filter({ $0.status == 1 })
                 self.chartView.clear()
-                self.chartView.models = [
-                    PieSliceModel(value: Double(completedTasks.count), color: .baseGreen),
-                    PieSliceModel(value: Double(tasks.count - completedTasks.count), color: .baseOrange),
-                ]
+                if tasks.isEmpty {
+                    self.chartView.models = [PieSliceModel(value: 1.0, color: .baseGreen)]
+                } else {
+                    self.chartView.models = [PieSliceModel(value: Double(tasks.count - completedTasks.count), color: .baseOrange), PieSliceModel(value: Double(completedTasks.count), color: .baseGreen)]
+                }
+                let tasksCount = "\(completedTasks.count)"
+                let tasksCompleted = "\ntasks completed"
+
+                let tasksCountAttributes: [NSAttributedString.Key: Any] = [
+                    .font: UIFont.medium(size: 38) ?? .systemFont(ofSize: 20)]
+                let taksCompletedAttributes: [NSAttributedString.Key: Any] = [
+                    .font: UIFont.regular(size: 10) ?? .systemFont(ofSize: 16)]
+                let attributedTasksCount = NSAttributedString(string: tasksCount, attributes: tasksCountAttributes)
+                let attributedTasksCompleted = NSAttributedString(string: tasksCompleted, attributes: taksCompletedAttributes)
+                let combinedAttributedString = NSMutableAttributedString()
+                combinedAttributedString.append(attributedTasksCount)
+                combinedAttributedString.append(attributedTasksCompleted)
+                self.completedTasksLabel.attributedText = combinedAttributedString
             }
             .store(in: &cancellables)
     }
